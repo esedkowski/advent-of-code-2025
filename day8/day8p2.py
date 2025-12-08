1fafc141e4519481020b55198cdb44c8955e1d10
esedkowski <esedkowski@gmail.com>
2025-12-08 18:58:58 +0100

    day8

---
+++ day8/day8p2.py
@@ -0,0 +1,113 @@
+import re
+import math
+
+INPUT = "input"
+TEST_INPUT = "test_input"
+
+file = TEST_INPUT
+file = INPUT
+
+def get_points(file):
+    with open(file) as _input:
+        points = []
+        for line in _input:
+            x, y, z = re.findall(r"\d+", line)
+            points.append((int(x), int(y), int(z)))
+
+    return points
+
+def calculate_distance(p1, p2):
+    sum = 0
+    for i in range(len(p1)):
+        sum += (p1[i] - p2[i]) ** 2
+    distance = math.sqrt(sum)
+
+    return distance
+
+
+def get_distances(points):
+    num_of_points = len(points)
+    distances = []
+    for i in range(num_of_points):
+        point1 = points[i]
+        for j in range(i + 1, num_of_points):
+            point2 = points[j]
+            distance = calculate_distance(point1, point2)
+            distances.append((i, j, distance))
+
+    return distances
+
+def sort_distances(distances):
+    num_of_distances = len(distances)
+    if num_of_distances < 2:
+        return distances
+
+    half_of_distances = num_of_distances // 2
+    half1 = sort_distances(distances[:half_of_distances])
+    half2 = sort_distances(distances[half_of_distances:])
+
+    sorted_distances = []
+    while len(half1) > 0 and len(half2) > 0:
+        if half1[0][2] < half2[0][2]:
+            sorted_distances.append(half1.pop(0))
+        else:
+            sorted_distances.append(half2.pop(0))
+    for i in range(len(half1)):
+        sorted_distances.append(half1.pop(0))
+    for i in range(len(half2)):
+        sorted_distances.append(half2.pop(0))
+
+    return(sorted_distances)
+
+def make_circuits(distances, num_of_points):
+    circuits = []
+    for distance in distances:
+        extended_circuits = []
+        for circuit in circuits:
+            if distance[0] in circuit and distance[1] in circuit:
+                extended_circuits.append(circuit)
+                break
+            elif distance[0] in circuit:
+                circuit.append(distance[1])
+                extended_circuits.append(circuit)
+            elif distance[1] in circuit:
+                circuit.append(distance[0])
+                extended_circuits.append(circuit)
+            else:
+                pass
+        if len(extended_circuits) == 0:
+            circuits.append([distance[0], distance[1]])
+        elif len(extended_circuits) == 1:
+            pass
+        else:
+            c1 = extended_circuits.pop()
+            c2 = extended_circuits.pop()
+            circuits.pop(circuits.index(c1))
+            circuits.pop(circuits.index(c2))
+            new_circuit = list(set(c1+c2))
+            circuits.append(new_circuit)
+        if len(circuits) == 1 and len(circuits[0]) == num_of_points:
+            last_pair = (distance[0], distance[1])
+            break
+
+    return circuits, last_pair
+
+def result(sorted_cir, num):
+    product = 1
+    biggest_cir = sorted_cir[:num]
+    for circuit in biggest_cir:
+        product *= len(circuit)
+    return product
+
+points = get_points(file)
+print(points[:3])
+distances = get_distances(points)
+print(distances[:3])
+sorted_dist = sort_distances(distances)
+print(sorted_dist[:5])
+
+circuits, last_pair = make_circuits(sorted_dist, len(points))
+p1, p2 = last_pair[0], last_pair[1]
+print(points[p1], points[p2])
+result = points[p1][0] * points[p2][0]
+print(result)
